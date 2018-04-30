fa1091128dca7c014c11414a28539d8c8216d7be
David LeBauer <dlebauer@illinois.edu>
2018-04-30 20:55:29 +0000

    pull methods table

--- cache-refresh.R
+++ cache-refresh.R
@@ -37,7 +37,7 @@ get_data_for_subexp <- function(subexp, exp_name) {
   traits_table <- tbl(bety_src, 'traits', n = Inf) %>%
     filter(date >= subexp[[ 'start_date' ]] & date <= subexp[[ 'end_date' ]]) %>%
     filter(site_id %in% site_ids) %>%
-    select(date, mean, variable_id, cultivar_id, treatment_id, site_id)
+    select(date, mean, variable_id, cultivar_id, treatment_id, site_id, method_id)
   
   n_traits <- traits_table %>% summarize(n = n()) %>% collect(n = Inf)
   if(n_traits == 0){
@@ -47,18 +47,26 @@ get_data_for_subexp <- function(subexp, exp_name) {
   sites_table <- tbl(bety_src, sql("select ST_AsText(sites.geometry) AS geometry, id from sites")) %>% 
     filter(!is.na(geometry)) %>% 
     filter(id %in% site_ids) %>%
-    rename(site_id = id)
+    dplyr::rename(site_id = id)
   
   cultivars_table <- tbl(bety_src, 'cultivars') %>%
+    # dplyr::rename(cultivar_id = id, cultivar_name = name) %>% 
     # workaround bug in dplyr https://github.com/tidyverse/dplyr/issues/2943 that causes bug in
-    # rename(cultivar_id = id, cultivar_name = name)
     rename(cultivar_id = id) %>% 
     rename(cultivar_name = name) %>%
     select(cultivar_id, cultivar_name) 
   
+  methods_table <- tbl(bety_src, 'methods') %>% 
+    dplyr::rename(method_id = id) %>% 
+    dplyr::rename(method = name) %>% 
+    select(method_id, method)
+    
+  
   traits <- traits_table %>% 
     left_join(sites_table, by = 'site_id') %>%
-    left_join(cultivars_table, by = 'cultivar_id') 
+    left_join(cultivars_table, by = 'cultivar_id') %>% 
+    left_join(methods_table, by = 'method_id')
+  
   
   variables <- tbl(bety_src, 'variables') %>%
     rename(variable_id = id)  %>% 
