e88115becfca5960dd1cc20609279701dd451e36
toddn <todd.nicholson@gmail.com>
2019-03-26 18:17:07 -0500

    fixing syntax error

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,13 @@
 case $1 in
 server)
   echo "IT IS A SERVER"
+  run Rscript app.R
   ;;
-  run Rscript cache-refresh.R
 
 update)
   echo "UPDATE DATA FILE"
-  ;;
   run Rscript cache-refresh.R
+  ;;
 *)
   exec "$@"
   ;;
