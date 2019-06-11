f6a19cd4464de960eecf8d87470b6cadbae0f52f
kimberlyh66 <kimberlyh9888@gmail.com>
2019-06-11 11:29:56 -0700

    speed up rendering of heat map; display only if user selects

--- render-site-map.R
+++ render-site-map.R
@@ -2,60 +2,70 @@ library(rgeos)
 library(dplyr)
 library(raster)
 library(mapview)
+library(sf)
+library(leafem)
 
 # render leaflet map from traits for a given date
-render_site_map <- function(traits, render_date, legend_title, image_path) {
+render_site_map <- function(traits, render_date, legend_title, image_path, display_heat_map) {
   
   if(!file.exists(image_path)){
     return()
   }else{
     
     # get most recent traits for each site
-    latest_traits <- subset(traits, date <= render_date & !is.na(geometry)) %>% 
-      group_by(geometry) %>% top_n(1, date)
+    # convert each site's geometry to a sfc object
+    latest_traits <- subset(traits, date <= render_date) %>% 
+      group_by(geometry) %>% 
+      top_n(1, date) %>% 
+      mutate(site_poly = st_as_sfc(geometry))
     
     pal <- colorNumeric(
       palette = 'Greens',
       domain = traits[[ 'mean' ]]
     )
     
-    map <- leaflet(options = leafletOptions(minZoom = 18, maxZoom = 21))  %>% 
+    map <- leaflet(options = leafletOptions(minZoom = 18, maxZoom = 21))  %>%
       addProviderTiles(providers$Esri.WorldImagery) 
+    
     # eventually want to overlay with stitched image from current day
     # see /data/terraref/sites/ua-mac/Level_1/fullfield/
     # cannot use addRasterImage (https://rstudio.github.io/leaflet/raster.html) for RasterStack 
-    # using multiband RGB thumbs 
+    # using multiband RGB thumbs, so use mapview::viewRGB() instead 
     
     # add polygon for each site, color by trait mean value
-    if (nrow(latest_traits) > 0) {
-      for (i in 1:nrow(latest_traits)){
-        curr_trait <- latest_traits[i,]
-        
-        site_poly <- readWKT(curr_trait[[ 'geometry' ]])
-        
-        if ('polygons' %in% names(attributes(site_poly))) {
-          trait_value <- curr_trait[[ 'mean' ]]
-          map <- addPolygons(map, data = site_poly, 
-                             color = pal(trait_value), opacity = 0, 
-                             fillColor = pal(trait_value), fillOpacity = 0.8,
-                             group = "Trait values")
-          #TODO add popup = plot name + trait value for plot 
-        }
-      }
+    # coerce data to multipolygon
+    if(display_heat_map == 'Yes'){
+      map <- addFeatures(map,
+                         data = st_cast(latest_traits[[ 'site_poly' ]], "MULTIPOLYGON"),
+                         color = pal(latest_traits[[ 'mean' ]]),
+                         opacity = 0,
+                         fillColor = pal(latest_traits[[ 'mean' ]]),
+                         fillOpacity = 0.8,
+                         group = 'Heat map')
+      
+      map <- addLayersControl(map,
+                              overlayGroups = "Heat map",
+                              position = "topright")
+    }else{
+      map <- addLayersControl(map, 
+                              position = "topright")
     }
     
-    map <- addLegend(map, "bottomright", pal = pal, 
+    map <- addLegend(map, "bottomright", 
+                     pal = pal, 
                      title = legend_title,
                      values = traits[[ 'mean' ]])
     
-    map <- addLayersControl(map, overlayGroups = "Trait values",
-                            position = "topright")
-    
     fullfield_image <- stack(image_path)
     
-    overlay_map <- viewRGB(x = fullfield_image, map = map, layer.name = 'full-field image')
+    map <- viewRGB(x = fullfield_image,
+                   map = map,
+                   layer.name = 'full-field image')
     
-    overlay_map@map
+    
+    map <- removeHomeButton(map)
+    
+    map
     
   }
   
