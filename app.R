b2edd0d962f13856ef499c8b2567d2c7724bc1b9
kimberlyh66 <kimberlyh9888@gmail.com>
2019-08-30 13:34:31 -0700

    create server for home page

--- app.R
+++ app.R
@@ -490,6 +490,22 @@ search_page <- fluidPage(theme = shinytheme('flatly'),
                          uiOutput('search_page_content')
 )
 
+# home page server
+home_server <- function(input, output, session) {
+  
+  # load 'full_cache_data' object from cache file
+  full_cache_data <- load_cache(full_cache_data)
+  
+  # render UI for all available experiments
+  output$home_page_content <- renderUI({
+    subexp_tabs <- lapply(names(full_cache_data), render_experiment_ui, full_cache_data)
+    do.call(tabsetPanel, subexp_tabs)
+  })
+  
+  # render outputs for all available experiments
+  lapply(names(full_cache_data), render_experiment_output, input, output, full_cache_data)
+}
+
 server <- function(input, output) {
   
   # load 'full_cache_data' object from cache file
