8ae051cd7d140fc29cd95446252d4c62cc447dec
kimberlyh66 <kimberlyh9888@gmail.com>
2019-08-30 14:27:24 -0700

    update render_plot for main page output

--- app.R
+++ app.R
@@ -124,7 +124,7 @@ render_cultivar_menu <- function(subexp_name, id_str, input, output, full_cache_
 
 # render box plot time series from trait records in a given subexperiment, for the selected variable
 # if a cultivar is selected, render line plot from trait records for that cultivar
-render_trait_plot <- function(subexp_name, id_str, input, output, full_cache_data) {
+render_plot <- function(subexp_name, id_str, input, output, full_cache_data) {
   
   output[[ paste0('trait_plot_', id_str) ]] <- renderPlot({
     
@@ -136,53 +136,12 @@ render_trait_plot <- function(subexp_name, id_str, input, output, full_cache_dat
     selected_cultivar <- input[[ paste0('selected_cultivar_', id_str) ]]
     
     plot_data <- selected_subexp_data[[ 'trait_data' ]][[ selected_variable ]][[ 'traits' ]]
-    data_max <- max(plot_data[[ 'mean' ]])
-    
     units <- selected_subexp_data[[ 'trait_data' ]][[ selected_variable ]][[ 'units' ]]
+    start_date <- as.Date(selected_subexp_data[[ 'start_date' ]])
+    end_date <- as.Date(selected_subexp_data[[ 'end_date' ]])
     
-    unique_vals <- unique(plot_data[[ 'mean' ]])
-    all_vals_integers <- all(unique_vals %% 1 == 0)
-    num_unique_vals <- length(unique_vals)
-
-    plot_data$method[is.na(plot_data$method) | plot_data$method == 'NA'] <- 'Not Provided'
-
-    trait_plot <- ggplot(data = plot_data, aes(x = as.Date(date), y = mean, color = method)) +
-      scale_colour_brewer(palette = "Set1")
-
-    {
-        if ((num_unique_vals < 20) & (max(unique_vals) < 30) & all_vals_integers) {
-          trait_plot <- trait_plot + 
-            geom_count() + 
-            geom_vline(aes(xintercept = as.numeric(as.Date(date))),
-                       color = 'grey')
-        } else {
-          trait_plot <- trait_plot + 
-            geom_violin(scale = 'width', width = 1, aes(group = as.Date(date))) +
-            geom_boxplot(outlier.alpha = 0.25, width = 0.2, aes(group = as.Date(date)))
-        }
-      }
-      
-    if (selected_cultivar != 'None') {
-        title <- paste0(selected_variable, '\nCultivar ', selected_cultivar, ' in red')
-        trait_plot <- trait_plot + 
-          geom_point(data = subset(plot_data, cultivar_name == selected_cultivar), 
-                     aes(x = as.Date(date), y = mean, group = site_id)) +
-          geom_line(data = subset(plot_data, cultivar_name == selected_cultivar), 
-                     size = 0.5, alpha = 0.5, aes(x = as.Date(date), y = mean, group = site_id)) 
-    } else {
-        title <- selected_variable
-    }
+    render_trait_plot(plot_data, selected_variable, selected_cultivar, units, start_date, end_date)
     
-    trait_plot + 
-      labs(
-        title = paste0(title, '\n'),
-        x = "Date",
-        y = units
-      ) +
-      theme_bw() + 
-      theme(text = element_text(size = 20), axis.text.x = element_text(angle = 45, hjust = 1)) +
-      xlim(as.Date(selected_subexp_data[[ 'start_date' ]]), as.Date(selected_subexp_data[[ 'end_date' ]])) +
-      ylim(0, data_max)
   })
 }
 
